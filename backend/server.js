bd3853f5d0dee36080e6652f0362d35b201c2ecf
parvinsorath1 <pguj6399@gmail.com>
2025-06-24 18:48:18 +0530

    Add root route to backend

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+      const bodyParser = require('body-parser');
+      const cors = require('cors');
 
-const app = express();
-const port = process.env.PORT || 3000;
+      const app = express();
+      const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cors({ origin: 'https://local1.vercel.app/' }));
+      app.use(bodyParser.json());
+      app.use(cors({ origin: '*' }));
 
-app.post('/save-data', (req, res) => {
-    const data = req.body;
-    console.log('Received Data:', data);
-    res.send('Data received!');
-});
+      app.get('/', (req, res) => {
+          res.send('Backend is running!');
+      });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+      app.post('/save-data', (req, res) => {
+          const data = req.body;
+          console.log('Received Data:', data);
+          res.send('Data received!');
+      });
+
+      app.listen(port, () => {
+          console.log(`Server is running on port ${port}`);
+      });
