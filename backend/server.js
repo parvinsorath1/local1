caf531189f820f0fc9b5d78500f634c2bf28f00c
parvinsorath1 <pguj6399@gmail.com>
2025-06-24 18:25:49 +0530

    Update server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://local1.vercel.app/' }));
 
 app.post('/save-data', (req, res) => {
     const data = req.body;
